fd45f75505eb313a7d232de16f83206b2d2c83b8
mayer <mayerlinserra24@gmail.com>
2024-04-26 16:31:30 -0300

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        //Console.WriteLine($"What is the magic number? ");
+        //int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomNumber = new Random();
+        int magicNumber = randomNumber.Next(1, 100);
+
+
+        int guess = 0;
+        int numGuesses = 0;
+
+        while (guess != magicNumber)
+        {Console.WriteLine(" What is your guess?");
+        guess = int.Parse(Console.ReadLine());
+        numGuesses++;
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Lower!");
+
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            Console.WriteLine($"Number of guesses: {numGuesses}");
+            
+        
+        }
+
+
+
     }
 }
